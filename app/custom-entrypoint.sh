96d46a0024a1a9868c5209199cb364866fbdbeaf
0x4r45h <0x4r45h@gmail.com>
2022-12-18 13:45:13 +0330

    add new flags to create validator in helper file format shell scripts

--- app/custom-entrypoint.sh
+++ app/custom-entrypoint.sh
@@ -3,30 +3,36 @@
 set -e
 
 _init_p2p() {
-    cd $HOME
-    rm -rf networks
-    rm -f $HOME/.celestia-app/config/genesis.json
-    git clone https://github.com/celestiaorg/networks.git
-    while true; do
-      # in case you want recover priv_validator_key.json through mnemonics
-        read -p "Do you want to import a previously created node mnemonics? " yn
-        case $yn in
-            [Yy]* ) celestia-appd init "$CELESTIA_NODE_NAME" --chain-id mocha --recover; break;;
-            [Nn]* ) celestia-appd init "$CELESTIA_NODE_NAME" --chain-id mocha; break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-    cp $HOME/networks/mocha/genesis.json $HOME/.celestia-app/config
+  cd $HOME
+  rm -rf networks
+  rm -f $HOME/.celestia-app/config/genesis.json
+  git clone https://github.com/celestiaorg/networks.git
+  while true; do
+    # in case you want recover priv_validator_key.json through mnemonics
+    read -p "Do you want to import a previously created node mnemonics? " yn
+    case $yn in
+    [Yy]*)
+      celestia-appd init "$CELESTIA_NODE_NAME" --chain-id mocha --recover
+      break
+      ;;
+    [Nn]*)
+      celestia-appd init "$CELESTIA_NODE_NAME" --chain-id mocha
+      break
+      ;;
+    *) echo "Please answer yes or no." ;;
+    esac
+  done
+  cp $HOME/networks/mocha/genesis.json $HOME/.celestia-app/config
 }
 _set_configs() {
-      if [ -z "${PEERS}" ]; then
-        # TODO : check the correctness of this with team
-         PEERS=$(curl -s https://rpc-mocha.pops.one/net_info | jq -r '.result.peers[] | select(.remote_ip | test("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$")) | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr | split(":")[-1])"' | tr '\n' ',' | sed 's/,$//')
-      fi
-      sed -i -e 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.celestia-app/config/config.toml
-      sed -i -e "s/^seed_mode *=.*/seed_mode = \"$SEED_MODE\"/" $HOME/.celestia-app/config/config.toml
-      # Make the node accessible outside of container
-      sed -i.bak -e "s/^laddr = \"tcp:\/\/127.0.0.1:26657\"/laddr = \"tcp:\/\/0.0.0.0:26657\"/" $HOME/.celestia-app/config/config.toml
+  if [ -z "${PEERS}" ]; then
+    # TODO : check the correctness of this with team
+    PEERS=$(curl -s https://rpc-mocha.pops.one/net_info | jq -r '.result.peers[] | select(.remote_ip | test("^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$")) | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr | split(":")[-1])"' | tr '\n' ',' | sed 's/,$//')
+  fi
+  sed -i -e 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.celestia-app/config/config.toml
+  sed -i -e "s/^seed_mode *=.*/seed_mode = \"$SEED_MODE\"/" $HOME/.celestia-app/config/config.toml
+  # Make the node accessible outside of container
+  sed -i.bak -e "s/^laddr = \"tcp:\/\/127.0.0.1:26657\"/laddr = \"tcp:\/\/0.0.0.0:26657\"/" $HOME/.celestia-app/config/config.toml
 }
 
 _config_pruning() {
@@ -50,75 +56,60 @@ _download_snapshot() {
     -C ~/.celestia-app/data/
 }
 _init_wallet() {
-    celestia-appd config keyring-backend test
-    while true; do
-        read -p "Do you want to import a previously created wallet? " yn
-        case $yn in
-            [Yy]* ) celestia-appd keys add $VALIDATOR_WALLET_NAME --recover; break;;
-            [Nn]* ) celestia-appd keys add $VALIDATOR_WALLET_NAME; break;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-    echo "celesvaloper address : "
-    celestia-appd keys show $VALIDATOR_WALLET_NAME --bech val -a
+  celestia-appd config keyring-backend test
+  while true; do
+    read -p "Do you want to import a previously created wallet? " yn
+    case $yn in
+    [Yy]*)
+      celestia-appd keys add $VALIDATOR_WALLET_NAME --recover
+      break
+      ;;
+    [Nn]*)
+      celestia-appd keys add $VALIDATOR_WALLET_NAME
+      break
+      ;;
+    *) echo "Please answer yes or no." ;;
+    esac
+  done
+  echo "celesvaloper address : "
+  celestia-appd keys show $VALIDATOR_WALLET_NAME --bech val -a
 }
-_get_wallet_balance() {
-    celestia-appd start > /dev/null 2>&1 &
-    echo "waiting for connection ..." && sleep 2
-    celestia-appd query bank balances $WALLET_ADDRESS
-}
-_validator_connect() {
-
-    celestia-appd tx staking create-validator \
-      --amount=1000000utia \
-      --pubkey="$(celestia-appd tendermint show-validator)" \
-      --moniker=$MONIKER \
-      --chain-id=mocha \
-      --evm-address=$EVM_ADDRESS \
-      --orchestrator-address=$ORCHESTRATOR_ADDRESS \
-      --commission-rate=0.1 \
-      --commission-max-rate=0.2 \
-      --commission-max-change-rate=0.01 \
-      --min-self-delegation=1000000 \
-      --from=$VALIDATOR_WALLET_NAME \
-      --keyring-backend=test
-}
-
 _init() {
-    _init_p2p
-    _set_configs
-    _config_pruning
-    _init_wallet
-while true; do
+  _init_p2p
+  _set_configs
+  _config_pruning
+  _init_wallet
+  while true; do
     read -p "Did you made a backup from your mnemonic keys? " yn
     case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) echo "Please, store it in a safe place then type yes";;
-        * ) echo "Please answer yes or no.";;
+    [Yy]*) break ;;
+    [Nn]*) echo "Please, store it in a safe place then type yes" ;;
+    *) echo "Please answer yes or no." ;;
     esac
-done
-while true; do
+  done
+  while true; do
     read -p "Do you want to download the latest snapshot? " yn
     case $yn in
-        [Yy]* ) _download_snapshot; break;;
-        [Nn]* ) exit ;;
-        * ) echo "Please answer yes or no.";;
+    [Yy]*)
+      _download_snapshot
+      break
+      ;;
+    [Nn]*) exit ;;
+    *) echo "Please answer yes or no." ;;
     esac
-done
+  done
 
 }
 if [ "$1" = 'debug' ]; then
-  trap : TERM INT; sleep infinity & wait
+  trap : TERM INT
+  sleep infinity &
+  wait
 elif [ "$1" = 'init' ]; then
-_init
-elif [ "$1" = 'wallet:balance' ]; then
-_get_wallet_balance
-elif [ "$1" = 'validator:connect' ]; then
-_validator_connect
+  _init
 elif [ "$1" = 'start' ]; then
   #always update configs before start to apply changes in .env file
-_set_configs
-/bin/celestia-appd start
+  _set_configs
+  /bin/celestia-appd start
 else
   /bin/celestia-appd "$@"
 fi
